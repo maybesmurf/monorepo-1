c57d8260dcb63807500032c49e2d9fc51a036e76
Anthony Shew <anthonyshew@gmail.com>
2022-01-06 14:38:19 -0700

    Make web happen.

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -31,6 +31,7 @@ const Home: NextPage = () => {
 
 	return (
 		<div>
+			<p>Test environment var: {process.env.TEST}</p>
 			<p style={{ marginBottom: "32px" }}>
 				Did we connect to the server?{" "}
 				{pingIsFetching ? <>Loading...</> : pingError ? <>Uh oh, we got problems.</> : <>{ping.message}</>}
