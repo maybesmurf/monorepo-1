ab3145b802dc6255f165e3120c78f3d042ca69f8
Anthony Shew <anthonyshew@gmail.com>
2022-01-16 14:15:30 -0700

    Add trials and clubs to v1.

--- api/src/routes/v1/index.ts
+++ api/src/routes/v1/index.ts
@@ -2,9 +2,13 @@ import express from "express"
 import { formatSuccess } from "../../utils/responseHandlers"
 const router = express.Router()
 
+import clubs from "./clubs"
 import dogs from "./dogs"
+import trials from "./trials"
 
+router.use("/clubs", clubs)
 router.use("/dogs", dogs)
+router.use("/trials", trials)
 
 // Ping v1
 router.get("/", async (req, response) => {
