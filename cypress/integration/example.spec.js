dfc560cca538b6480ab561fd9ce15c726b18cc4e
Anthony Shew <anthonyshew@gmail.com>
2022-01-18 16:27:26 -0700

    Try all the ports I can think of.

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -6,7 +6,19 @@ describe("The Home Page", () => {
 });
 
 describe("The API", () => {
-  it("pings", () => {
+  it("pings http://rest_api", () => {
+    cy.request("http://rest_api");
+  });
+
+  it("pings http://rest_api:5000", () => {
     cy.request("http://rest_api:5000");
+  });
+
+  it("pings http://localhost:5000", () => {
+    cy.request("http://localhost:5000");
+  });
+
+  it("pings http://0.0.0.0:5000", () => {
+    cy.request("http://0.0.0.0:5000");
   });
 });
