99cddd83d8d6eac62aa8d7549c229c2d1adbffe9
Anthony Shew <anthonyshew@gmail.com>
2022-01-18 17:57:44 -0700

    WE PASS LIKE THE LEGENDS WE ARE

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -6,19 +6,7 @@ describe("The Home Page", () => {
 });
 
 describe("The API", () => {
-  it("pings http://rest_api", () => {
-    cy.request("http://rest_api");
-  });
-
   it("pings http://rest_api:5000", () => {
     cy.request("http://rest_api:5000");
-  });
-
-  it("pings http://localhost:5000", () => {
-    cy.request("http://localhost:5000");
-  });
-
-  it("pings http://0.0.0.0:5000", () => {
-    cy.request("http://0.0.0.0:5000");
   });
 });
