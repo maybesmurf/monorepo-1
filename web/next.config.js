49fc80b5e827a95aa562a6b8b088d0a204df029d
Anthony Shew <anthonyshew@gmail.com>
2022-01-24 17:14:00 -0700

    Speed up next dev environment.

--- web/next.config.js
+++ web/next.config.js
@@ -9,8 +9,9 @@ const moduleExports = {
 	reactStrictMode: true,
 	webpackDevMiddleware: (config) => {
 		config.watchOptions = {
-			poll: 1000,
-			aggregateTimeout: 300
+			poll: 800,
+			aggregateTimeout: 300,
+			ignored: /node_modules/
 		}
 
 		return config
