7b7b38c8f834bc5a3a8d865633edbeb8d5ae42d0
Anthony Shew <anthonyshew@gmail.com>
2022-01-25 22:36:54 -0700

    Fix pathing for compilation.

--- web/pages/test-api.tsx
+++ web/pages/test-api.tsx
@@ -49,34 +49,19 @@ const Home: NextPage = () => {
 			<Container>
 				<Select
 					size="xs"
+					radius="xl"
 					initiallyOpened
 					label="Choose employee of the month"
 					placeholder="Pick one"
 					data={[
-						{
-							label: "Bender Bending Rodríguez",
-							value: "Bender Bending Rodríguez"
-						},
-						{
-							label: "Carol Miller",
-							value: "Carol Miller"
-						},
-						{
-							label: "Homer Simpson",
-							value: "Homer Simpson"
-						},
-						{
-							label: "Spongebob Squarepants",
-							value: "Spongebob Squarepants"
-						}
+						{ value: "rick", label: "Rick", group: "Used to be a pickle" },
+						{ value: "morty", label: "Morty", group: "Never was a pickle" },
+						{ value: "beth", label: "Beth", group: "Never was a pickle" },
+						{ value: "summer", label: "Summer", group: "Never was a pickle" }
 					]}
 					searchable
 					maxDropdownHeight={400}
 					nothingFound="Nobody here"
-					// filter={(value, item) =>
-					// 	item.label.toLowerCase().includes(value.toLowerCase().trim()) ||
-					// 	item.description.toLowerCase().includes(value.toLowerCase().trim())
-					// }
 				/>
 			</Container>
 		</div>
