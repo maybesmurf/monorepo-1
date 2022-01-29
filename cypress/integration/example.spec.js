936d20fd334040efe83ec802fda7ed08dc067fff
Anthony Shew <anthonyshew@gmail.com>
2022-01-28 17:14:17 -0700

    Prepare Cypress.

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,12 +1,6 @@
 describe("The Home Page", () => {
   it("renders", () => {
-    cy.visit("http://web:3000");
+    cy.visit("/");
     cy.contains("Your dog's sporting event, simplified.");
-  });
-});
-
-describe("The API", () => {
-  it("pings http://rest_api:5000", () => {
-    cy.request("http://rest_api:5000");
   });
 });
