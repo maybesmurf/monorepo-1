f3e4d2ef671cc0590500c29cb1ffe54c15d91b36
Anthony Shew <anthonyshew@gmail.com>
2022-02-06 15:02:53 -0700

    Groundwork for integration tests of dog service.

--- api/src/v1/services/dog/dog.int.test.ts
+++ api/src/v1/services/dog/dog.int.test.ts
@@ -1,3 +1,4 @@
+// TODO: Absolute import
 import { buildDog } from "../../../../.jest/dogGenerator"
 import DogService from "./index"
 import { prisma } from "@Libs/prisma"
@@ -9,8 +10,8 @@ describe("Dog Services", () => {
 	faker.seed(123)
 
 	const dog1 = buildDog({ id: faker.datatype.uuid(), callName: "BBB" })
-	const dog2 = buildDog({ id: faker.datatype.uuid(), callName: "AAA" })
-	const dog3 = buildDog({ id: faker.datatype.uuid(), callName: "DDD" })
+	const dog2 = buildDog({ id: faker.datatype.uuid(), callName: "AAA", sex: "FEMALE" })
+	const dog3 = buildDog({ id: faker.datatype.uuid(), callName: "DDD", sex: "FEMALE" })
 	const dog4 = buildDog({ id: faker.datatype.uuid(), callName: "CCC" })
 
 	const createDog = buildDog()
@@ -19,6 +20,8 @@ describe("Dog Services", () => {
 	const { id: dogId } = dog1
 
 	beforeEach(async () => {
+		// This is weird, why do I have to do this deleteMany?
+		await prisma.dog.deleteMany({ where: { id: { in: [dog1.id, dog2.id, dog3.id, dog4.id, createDog.id] } } })
 		await prisma.dog.createMany({ data: [dog1, dog2, dog3, dog4] })
 	})
 
@@ -32,14 +35,24 @@ describe("Dog Services", () => {
 			expect(dogResult).toEqual(dog1)
 		})
 
-		it("list dogs", async () => {
-			const listResult = await dogService.list({ where: {}, skip: 0, take: 10 })
-			expect(listResult).toEqual([dog3, dog4, dog2, dog1])
+		it("list only female dogs", async () => {
+			const listResult = await dogService.list({ where: { sex: "FEMALE" }, skip: 0, take: 10 })
+			expect(listResult).toEqual(expect.arrayContaining([dog2, dog3]))
 		})
 
 		it("sort dogs by call name", async () => {
 			const listResult = await dogService.list({ where: {}, skip: 0, take: 10, orderBy: { callName: "asc" } })
-			expect(listResult).toEqual([dog2, dog1, dog4, dog3])
+
+			// Filter to only test dogs
+			const filteredResult = listResult.filter(
+				(dog) => dog.id === dog1.id || dog.id === dog2.id || dog.id === dog3.id || dog.id === dog4.id
+			)
+
+			// Pare down to just the call names for comparison
+			const resultCallNames = filteredResult.map((dog) => dog.callName)
+			const testCallNames = [dog1, dog2, dog3, dog4].map((dog) => dog.callName).sort()
+
+			expect(resultCallNames).toEqual(testCallNames)
 		})
 
 		it("creates a dog", async () => {
