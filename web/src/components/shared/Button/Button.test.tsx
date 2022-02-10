918ba9afc181293e75b49f2d7682562437c5fc05
Anthony Shew <anthonyshew@gmail.com>
2022-02-10 16:44:37 -0700

    Button tests.

--- web/src/components/shared/Button/Button.test.tsx
+++ web/src/components/shared/Button/Button.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "./test-utils"
+import { render, screen, getComputedStyle } from "./test-utils"
 import { Button } from "./index"
 
 describe("Button", () => {
@@ -15,6 +15,28 @@ describe("Button", () => {
 			</Button>
 		)
 
-		expect(screen.getByText("Test Text").closest("a")).toBeInTheDocument()
+		expect(screen.getByText("Test Text").closest("a")).not.toBeDisabled()
+	})
+
+	it("Uses md radius on xs size", async () => {
+		render(
+			<Button id="button" size="xs">
+				Test Text
+			</Button>
+		)
+
+		const style = getComputedStyle("button")
+		expect(style.borderRadius).toBe("8px")
+	})
+
+	it("Uses lg radius on sm and up", async () => {
+		render(
+			<Button id="button" size="sm">
+				Test Text
+			</Button>
+		)
+
+		const style = getComputedStyle("button")
+		expect(style.borderRadius).toBe("16px")
 	})
 })
--- web/src/components/shared/Button/test-utils.tsx
+++ web/src/components/shared/Button/test-utils.tsx
@@ -12,3 +12,9 @@ const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "wrapper">
 
 export * from "@testing-library/react"
 export { customRender as render }
+
+export const getComputedStyle = (id: string) => {
+	const elem = document.getElementById(id)
+	if (!elem) throw new Error("No element!")
+	return window.getComputedStyle(elem)
+}
